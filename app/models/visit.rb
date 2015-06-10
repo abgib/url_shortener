0323288e51b751d39ddaaaba957b8bd6c7f49824
Steve+Charles <Steve+Charles@appacademy.io>
2015-06-10 15:18:32 -0400

    Add Visit#record_visit! factory method.

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,11 @@
 class Visit < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :url_id, :presence => true
+
+  def self.record_visit!(user, shortened_url)
+    Visit.new(
+      :user_id => user.id,
+      :url_id  => shortened_url.id
+    )
+  end
 end
